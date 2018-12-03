1fc2773689f4ff9526191b56af6b74129b41ec96
meghana chowdary panguluri <meghanachowdary55@gmail.com>
2018-12-02 23:40:56 -0500

    Add project files.

---
+++ StockPickingStrategy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StockPickingStrategy.MvcApplication" Language="C#" %>
---
+++ StockPickingStrategy/Models/Equity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockPickingStrategy.Models
+{
+    public class Equity
+    {
+        public int EquityId { get; set; }
+        public string date { get; set; }
+        public float open { get; set; }
+        public float high { get; set; }
+        public float low { get; set; }
+        public float close { get; set; }
+        public int volume { get; set; }
+        public int unadjustedVolume { get; set; }
+        public float change { get; set; }
+        public float changePercent { get; set; }
+        public float vwap { get; set; }
+        public string label { get; set; }
+        public float changeOverTime { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class ChartRoot
+    {
+        public Equity[] chart { get; set; }
+    }
+}
